3955ea914408b91852ff3e1bc99b858aeae74eda
Per Lönn Wege <per@perlw.se>
2018-12-16 20:44:03 +0100

    WIP 15, we've got movement

--- 2018/15/main.go
+++ 2018/15/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 
 	"github.com/fatih/color"
 )
@@ -23,7 +24,25 @@ const (
 	TileIDGoblin
 )
 
+func (t TileID) String() string {
+	switch t {
+	case TileIDUnknown:
+		return "unknown"
+	case TileIDGround:
+		return "ground"
+	case TileIDWall:
+		return "wall"
+	case TileIDElf:
+		return "elf"
+	case TileIDGoblin:
+		return "goblin"
+	default:
+		return "n/a"
+	}
+}
+
 type TileDef struct {
+	ID    TileID
 	Char  rune
 	Color *color.Color
 }
@@ -34,14 +53,17 @@ func (t TileDef) String() string {
 
 var TileMap = map[TileID]*TileDef{
 	TileIDUnknown: &TileDef{
+		ID:    TileIDUnknown,
 		Char:  '?',
 		Color: color.New(color.FgBlack).Add(color.BgMagenta),
 	},
 	TileIDGround: &TileDef{
+		ID:    TileIDGround,
 		Char:  '.',
 		Color: color.New(color.FgYellow),
 	},
 	TileIDWall: &TileDef{
+		ID:    TileIDWall,
 		Char:  '#',
 		Color: color.New(color.FgHiWhite),
 	},
@@ -49,10 +71,12 @@ var TileMap = map[TileID]*TileDef{
 
 var CreatureMap = map[TileID]*TileDef{
 	TileIDElf: &TileDef{
+		ID:    TileIDElf,
 		Char:  'E',
 		Color: color.New(color.FgRed),
 	},
 	TileIDGoblin: &TileDef{
+		ID:    TileIDGoblin,
 		Char:  'G',
 		Color: color.New(color.FgGreen),
 	},
@@ -76,6 +100,7 @@ type Creature interface {
 	GetPos() (int, int)
 	GetPower() int
 	GetTile() *TileDef
+	Move(int, int)
 }
 
 func NewCreature(tileID TileID, x, y int) Creature {
@@ -122,6 +147,11 @@ func (e *Elf) GetTile() *TileDef {
 	return e.Tile
 }
 
+func (e *Elf) Move(dx, dy int) {
+	e.X += dx
+	e.Y += dy
+}
+
 type Goblin struct {
 	X, Y  int
 	Hp    int
@@ -155,9 +185,15 @@ func (g *Goblin) GetTile() *TileDef {
 	return g.Tile
 }
 
+func (g *Goblin) Move(dx, dy int) {
+	g.X += dx
+	g.Y += dy
+}
+
+var GridWidth int
+var GridSize int
+
 type Puzzle struct {
-	GridWidth int
-	GridSize  int
 	World     []Tile
 	Creatures []Creature
 }
@@ -178,7 +214,7 @@ func abs(v int) int {
 	return v
 }
 
-func (p *Puzzle) flood(reach []rune, sx, sy int, dir FloodDir) {
+func (p *Puzzle) flood(reach []rune, sx, sy int, dir FloodDir, creature TileID) {
 	switch dir {
 	case FloodDirN:
 		sy--
@@ -190,11 +226,11 @@ func (p *Puzzle) flood(reach []rune, sx, sy int, dir FloodDir) {
 		sx++
 	}
 
-	if sx < 0 || sy < 0 || sx > p.GridWidth-1 || sy > p.GridWidth-1 {
+	if sx < 0 || sy < 0 || sx > GridWidth-1 || sy > GridWidth-1 {
 		return
 	}
 
-	i := (sy * p.GridWidth) + sx
+	i := (sy * GridWidth) + sx
 	if reach[i] == '+' || p.World[i].Def.Char == '#' {
 		return
 	}
@@ -202,12 +238,18 @@ func (p *Puzzle) flood(reach []rune, sx, sy int, dir FloodDir) {
 	for _, c := range p.Creatures {
 		cx, cy := c.GetPos()
 		if sx == cx && sy == cy {
-			reach[i] = 'X'
+			if c.GetTile().ID != creature {
+				reach[i] = 'X'
+			} else {
+				reach[i] = '-'
+			}
 			return
 		}
-		d := abs(cx-sx) + abs(cy-sy)
-		if d < 2 {
-			close = true
+		if c.GetTile().ID != creature {
+			d := abs(cx-sx) + abs(cy-sy)
+			if d < 2 {
+				close = true
+			}
 		}
 	}
 
@@ -219,62 +261,196 @@ func (p *Puzzle) flood(reach []rune, sx, sy int, dir FloodDir) {
 
 	switch dir {
 	case FloodDirN:
-		p.flood(reach, sx, sy, FloodDirN)
-		p.flood(reach, sx, sy, FloodDirW)
-		p.flood(reach, sx, sy, FloodDirE)
+		p.flood(reach, sx, sy, FloodDirN, creature)
+		p.flood(reach, sx, sy, FloodDirW, creature)
+		p.flood(reach, sx, sy, FloodDirE, creature)
 	case FloodDirS:
-		p.flood(reach, sx, sy, FloodDirS)
-		p.flood(reach, sx, sy, FloodDirW)
-		p.flood(reach, sx, sy, FloodDirE)
+		p.flood(reach, sx, sy, FloodDirS, creature)
+		p.flood(reach, sx, sy, FloodDirW, creature)
+		p.flood(reach, sx, sy, FloodDirE, creature)
 	case FloodDirW:
-		p.flood(reach, sx, sy, FloodDirN)
-		p.flood(reach, sx, sy, FloodDirS)
-		p.flood(reach, sx, sy, FloodDirW)
+		p.flood(reach, sx, sy, FloodDirN, creature)
+		p.flood(reach, sx, sy, FloodDirS, creature)
+		p.flood(reach, sx, sy, FloodDirW, creature)
 	case FloodDirE:
-		p.flood(reach, sx, sy, FloodDirN)
-		p.flood(reach, sx, sy, FloodDirS)
-		p.flood(reach, sx, sy, FloodDirE)
+		p.flood(reach, sx, sy, FloodDirN, creature)
+		p.flood(reach, sx, sy, FloodDirS, creature)
+		p.flood(reach, sx, sy, FloodDirE, creature)
 	}
 }
 
-func (p *Puzzle) floodFillReach(sx, sy int) []rune {
-	reach := make([]rune, p.GridSize)
-	for y := 0; y < p.GridWidth; y++ {
-		for x := 0; x < p.GridWidth; x++ {
-			i := (y * p.GridWidth) + x
+func (p *Puzzle) floodFillReach(sx, sy int, creature TileID) []rune {
+	reach := make([]rune, GridSize)
+	for y := 0; y < GridWidth; y++ {
+		for x := 0; x < GridWidth; x++ {
+			i := (y * GridWidth) + x
 			reach[i] = ' '
 		}
 	}
 
-	p.flood(reach, sx, sy, FloodDirN)
-	p.flood(reach, sx, sy, FloodDirS)
-	p.flood(reach, sx, sy, FloodDirW)
-	p.flood(reach, sx, sy, FloodDirE)
+	p.flood(reach, sx, sy, FloodDirN, creature)
+	p.flood(reach, sx, sy, FloodDirS, creature)
+	p.flood(reach, sx, sy, FloodDirW, creature)
+	p.flood(reach, sx, sy, FloodDirE, creature)
 
 	return reach
 }
 
-func (p *Puzzle) Solution1(pretty bool) int {
-	p.PrintState()
+type ByCoord []Creature
 
-	fmt.Printf("\n")
-	reach := p.floodFillReach(1, 1)
-	for y := 0; y < p.GridWidth; y++ {
-		for x := 0; x < p.GridWidth; x++ {
-			i := (y * p.GridWidth) + x
-			fmt.Printf("%c", reach[i])
+func (c ByCoord) Len() int {
+	return len(c)
+}
+
+func (c ByCoord) Swap(i, j int) {
+	c[i], c[j] = c[j], c[i]
+}
+
+func (c ByCoord) Less(i, j int) bool {
+	cax, cay := c[i].GetPos()
+	cbx, cby := c[j].GetPos()
+	ia := (cay * GridWidth) + cax
+	ib := (cby * GridWidth) + cbx
+	return ia < ib
+}
+
+type Point []int
+
+func (p *Puzzle) Solution1(pretty bool) int {
+	turn := 1
+	for {
+		fmt.Printf("\n### Turn %d ###\n", turn)
+		for _, c := range p.Creatures {
+			fmt.Printf("%s,", c.GetTile().ID)
 		}
 		fmt.Printf("\n")
+
+		// fmt.Printf("Turn order: ")
+		sort.Sort(ByCoord(p.Creatures))
+		for _, c := range p.Creatures {
+			fmt.Printf("%s,", c.GetTile().ID)
+		}
+		fmt.Printf("\n")
+
+		for t, c := range p.Creatures {
+			fmt.Printf("\n=== #%d, %s ===\n", t, c.GetTile().ID)
+
+			p.PrintState()
+
+			fmt.Printf("\n")
+			cx, cy := c.GetPos()
+			reach := p.floodFillReach(cx, cy, c.GetTile().ID)
+			for y := 0; y < GridWidth; y++ {
+				for x := 0; x < GridWidth; x++ {
+					i := (y * GridWidth) + x
+					fmt.Printf("%c", reach[i])
+				}
+				fmt.Printf("\n")
+			}
+
+			nextToTarget := -1
+			targets := make([]Creature, 0, len(reach))
+			positions := make([]Point, 0, len(reach))
+			for y := 0; y < GridWidth; y++ {
+				for x := 0; x < GridWidth; x++ {
+					i := (y * GridWidth) + x
+					if reach[i] == 'X' {
+						for t, cc := range p.Creatures {
+							cxx, cyy := cc.GetPos()
+							if x == cxx && y == cyy {
+								if nextToTarget < 0 {
+									d := abs(cx-cxx) + abs(cy-cyy)
+									if d < 2 {
+										nextToTarget = t
+									}
+								}
+
+								targets = append(targets, cc)
+							}
+						}
+					} else if reach[i] == '!' {
+						positions = append(positions, Point{x, y})
+					}
+				}
+			}
+
+			if len(targets) == 0 {
+				fmt.Println("ALL DEAD")
+				return 0
+			}
+
+			fmt.Println(len(targets), targets)
+			fmt.Println(len(positions), positions)
+
+			fmt.Println("next to:", nextToTarget)
+			if nextToTarget < 0 {
+				// n, w, e, s
+				lowest := 100
+				dir := -1
+				dirs := []int{100, 100, 100, 100}
+				for _, p := range positions {
+					var d int
+					d = abs(cx-p[0]) + abs(cy-p[1]-1)
+					if d < dirs[0] {
+						dirs[0] = d
+					}
+					if d < lowest {
+						lowest = d
+						dir = 0
+					}
+					d = abs(cx-p[0]-1) + abs(cy-p[1])
+					if d < dirs[1] {
+						dirs[1] = d
+					}
+					if d < lowest {
+						lowest = d
+						dir = 1
+					}
+					d = abs(cx-p[0]+1) + abs(cy-p[1])
+					if d < dirs[2] {
+						dirs[2] = d
+					}
+					if d < lowest {
+						lowest = d
+						dir = 2
+					}
+					d = abs(cx-p[0]) + abs(cy-p[1]+1)
+					if d < dirs[3] {
+						dirs[3] = d
+					}
+					if d < lowest {
+						lowest = d
+						dir = 3
+					}
+				}
+
+				switch dir {
+				case 0:
+					c.Move(0, -1)
+				case 1:
+					c.Move(-1, 0)
+				case 2:
+					c.Move(1, 0)
+				case 3:
+					c.Move(0, 1)
+				}
+			}
+
+			if nextToTarget > -1 {
+			}
+
+			waitEnter()
+		}
+		turn++
 	}
-	waitEnter()
 
 	return 0
 }
 
 func (p *Puzzle) PrintState() {
-	for y := 0; y < p.GridWidth; y++ {
-		for x := 0; x < p.GridWidth; x++ {
-			i := (y * p.GridWidth) + x
+	for y := 0; y < GridWidth; y++ {
+		for x := 0; x < GridWidth; x++ {
+			i := (y * GridWidth) + x
 			found := false
 			for _, c := range p.Creatures {
 				cx, cy := c.GetPos()
@@ -337,8 +513,8 @@ func main() {
 	if err := scanner.Err(); err != nil {
 		panic(err.Error())
 	}
-	p.GridWidth = lines
-	p.GridSize = p.GridWidth * p.GridWidth
+	GridWidth = lines
+	GridSize = GridWidth * GridWidth
 
 	fmt.Println(p.Solution1(true))
 }
