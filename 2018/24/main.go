8fa429a9b26041674ddb0c22692cc67edccc2c72
Per Lönn Wege <per@perlw.se>
2018-12-28 20:26:34 +0100

    Initial input parsing

--- 2018/24/main.go
+++ 2018/24/main.go
@@ -4,7 +4,20 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
+
+	"github.com/davecgh/go-spew/spew"
 )
+
+type Group struct {
+	Amount     int
+	HP         int
+	Weak       []string
+	Immune     []string
+	Damage     int
+	DamageType string
+	Initiative int
+}
 
 func waitEnter() {
 	fmt.Printf("Press ENTER to continue\n")
@@ -46,10 +59,15 @@ type Point struct {
 }
 
 type Puzzle struct {
+	ImmuneSystem []Group
+	Infection    []Group
 }
 
 func NewPuzzle(filepath string) *Puzzle {
-	p := Puzzle{}
+	p := Puzzle{
+		ImmuneSystem: make([]Group, 0, 10),
+		Infection:    make([]Group, 0, 10),
+	}
 
 	input, err := os.Open(filepath)
 	if err != nil {
@@ -57,24 +75,82 @@ func NewPuzzle(filepath string) *Puzzle {
 	}
 	scanner := bufio.NewScanner(input)
 
-	for scanner.Scan() {
-		var x, y, z, r int
-		fmt.Sscanf(scanner.Text(), "pos=<%d,%d,%d>, r=%d", &x, &y, &z, &r)
+	target := p.ImmuneSystem
+	for t := 0; t < 2; t++ {
+		scanner.Scan()
+		if err := scanner.Err(); err != nil {
+			panic(err.Error())
+		}
+		for scanner.Scan() {
+			var num, hp, pow, init int
+			immune := make([]string, 0, 4)
+			weak := make([]string, 0, 4)
+			var damagetype string
+
+			line := scanner.Text()
+			if line == "" {
+				scanner.Scan()
+				scanner.Scan()
+				p.ImmuneSystem = target
+				target = p.Infection
+				continue
+			}
+
+			parts := strings.FieldsFunc(line, func(c rune) bool {
+				return c == '(' || c == ')'
+			})
+
+			vparts := strings.Split(parts[1], ";")
+			for _, vul := range vparts {
+				vul = strings.Trim(vul, " ")
+				vulp := strings.FieldsFunc(vul, func(c rune) bool {
+					return c == ' ' || c == ','
+				})
+				for t := 2; t < len(vulp); t++ {
+					switch vulp[0] {
+					case "weak":
+						weak = append(weak, vulp[t])
+					case "immune":
+						immune = append(immune, vulp[t])
+					}
+				}
+			}
+
+			fmt.Sscanf(parts[0], "%d units each with %d hit points", &num, &hp)
+			fmt.Sscanf(parts[2], " with an attack that does %d %s damage at initiative %d", &pow, &damagetype, &init)
+
+			target = append(target, Group{
+				Amount:     num,
+				Damage:     pow,
+				DamageType: damagetype,
+				HP:         hp,
+				Immune:     immune,
+				Initiative: init,
+				Weak:       weak,
+			})
+		}
+		if err := scanner.Err(); err != nil {
+			panic(err.Error())
+		}
 	}
-	if err := scanner.Err(); err != nil {
-		panic(err.Error())
-	}
+	p.Infection = target
+
+	spew.Dump(p)
+
 	input.Close()
 
 	return &p
 }
 
+func (p *Puzzle) Sim() {
+}
+
 func main() {
 	// Test1
 	t := NewPuzzle("test1.txt")
+	t.Sim()
 
 	// Puzzle1
 	/*p := NewPuzzle("input.txt")
-	fmt.Println("In range:", len(p.DetectInRangeBot(p.FindStrongestBot())))
-	fmt.Printf("Overlapping dist: %+v\n", p.FindMostOverlapDist())*/
+	p.Sim()*/
 }
