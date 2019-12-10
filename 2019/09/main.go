c2829dffcaa6b7a01a018f2fde41d781d14640be
Per Lönn Wege <per@perlw.se>
2019-12-10 13:27:18 +0100

    re-use

--- 2019/09/main.go
+++ 2019/09/main.go
@@ -7,221 +7,9 @@ import (
 	"os"
 	"strconv"
 	"strings"
+
+	"github.com/perlw/advent_of_code/2019/intcode"
 )
-
-type OpType int
-
-const (
-	OpAdd OpType = iota + 1
-	OpMul
-	OpInput
-	OpOutput
-	OpJumpTrue
-	OpJumpFalse
-	OpLessThan
-	OpEquals
-	OpRelativeBase
-	OpEnd = 99
-)
-
-type ModeType int
-
-const (
-	ModePosition ModeType = iota
-	ModeImmediate
-	ModeRelative
-)
-
-type Computer struct {
-	mem    []int
-	pc     int
-	rb     int
-	Halted bool
-}
-
-func NewComputer(initial []int) *Computer {
-	com := Computer{}
-	com.mem = make([]int, 65535)
-	copy(com.mem, initial)
-	return &com
-}
-
-func (com *Computer) getArgPos(pos int, mode ModeType) (int, error) {
-	if pos < 0 || pos >= len(com.mem) {
-		return -1, fmt.Errorf("invalid index, out of range: %d", pos)
-	}
-	switch mode {
-	case ModePosition:
-		if com.mem[pos] < 0 || com.mem[pos] >= len(com.mem) {
-			return -1, fmt.Errorf("invalid position index, out of range: %d", com.mem[pos])
-		}
-		return com.mem[pos], nil
-	case ModeRelative:
-		rel := com.mem[pos] + com.rb
-		if rel < 0 || rel >= len(com.mem) {
-			return -1, fmt.Errorf("invalid relative index, out of range: %d", rel)
-		}
-		return rel, nil
-	}
-	return pos, nil
-}
-
-func (com *Computer) Iterate(input func() int, yieldOnInput bool, debug bool) (int, bool, error) {
-	log := ioutil.Discard
-	if debug {
-		log = os.Stdout
-	}
-	for com.pc < len(com.mem) {
-		op := OpType(com.mem[com.pc])
-		if op == OpEnd {
-			fmt.Fprintf(log, "Halt!\n")
-			com.Halted = true
-			return 0, true, nil
-		}
-
-		aMode := ModePosition
-		bMode := ModePosition
-		cMode := ModePosition
-		if op > 99 {
-			aMode = ModeType((op / 100) % 10)
-			bMode = ModeType((op / 1000) % 10)
-			cMode = ModeType((op / 10000) % 10)
-			op %= 100
-		}
-
-		switch op {
-		case OpAdd:
-			a, err := com.getArgPos(com.pc+1, aMode)
-			if err != nil {
-				return 0, false, err
-			}
-			b, err := com.getArgPos(com.pc+2, bMode)
-			if err != nil {
-				return 0, false, err
-			}
-			o, err := com.getArgPos(com.pc+3, cMode)
-			if err != nil {
-				return 0, false, err
-			}
-			com.mem[o] = com.mem[a] + com.mem[b]
-			fmt.Fprintf(log, "Setting %d @%d\n", com.mem[o], o)
-			com.pc += 4
-		case OpMul:
-			a, err := com.getArgPos(com.pc+1, aMode)
-			if err != nil {
-				return 0, false, err
-			}
-			b, err := com.getArgPos(com.pc+2, bMode)
-			if err != nil {
-				return 0, false, err
-			}
-			o, err := com.getArgPos(com.pc+3, cMode)
-			if err != nil {
-				return 0, false, err
-			}
-			com.mem[o] = com.mem[a] * com.mem[b]
-			fmt.Fprintf(log, "Setting %d @%d\n", com.mem[o], o)
-			com.pc += 4
-		case OpInput:
-			a, err := com.getArgPos(com.pc+1, aMode)
-			if err != nil {
-				return 0, false, err
-			}
-			com.mem[a] = input()
-			fmt.Fprintf(log, "Inputting: %d @%d\n", com.mem[a], a)
-			com.pc += 2
-			if yieldOnInput {
-				return 0, false, nil
-			}
-		case OpOutput:
-			a, err := com.getArgPos(com.pc+1, aMode)
-			if err != nil {
-				return 0, false, err
-			}
-			fmt.Fprintf(log, "Outputting: %d @%d\n", com.mem[a], a)
-			com.pc += 2
-			return com.mem[a], false, nil
-		case OpJumpTrue:
-			a, err := com.getArgPos(com.pc+1, aMode)
-			if err != nil {
-				return 0, false, err
-			}
-			b, err := com.getArgPos(com.pc+2, bMode)
-			if err != nil {
-				return 0, false, err
-			}
-			if com.mem[a] != 0 {
-				com.pc = com.mem[b]
-				fmt.Fprintf(log, "Jump to %d\n", com.pc)
-			} else {
-				com.pc += 3
-			}
-		case OpJumpFalse:
-			a, err := com.getArgPos(com.pc+1, aMode)
-			if err != nil {
-				return 0, false, err
-			}
-			b, err := com.getArgPos(com.pc+2, bMode)
-			if err != nil {
-				return 0, false, err
-			}
-			if com.mem[a] == 0 {
-				com.pc = com.mem[b]
-				fmt.Fprintf(log, "Jump to %d\n", com.pc)
-			} else {
-				com.pc += 3
-			}
-		case OpLessThan:
-			a, err := com.getArgPos(com.pc+1, aMode)
-			if err != nil {
-				return 0, false, err
-			}
-			b, err := com.getArgPos(com.pc+2, bMode)
-			if err != nil {
-				return 0, false, err
-			}
-			o, err := com.getArgPos(com.pc+3, cMode)
-			if err != nil {
-				return 0, false, err
-			}
-			if com.mem[a] < com.mem[b] {
-				com.mem[o] = 1
-			} else {
-				com.mem[o] = 0
-			}
-			com.pc += 4
-		case OpEquals:
-			a, err := com.getArgPos(com.pc+1, aMode)
-			if err != nil {
-				return 0, false, err
-			}
-			b, err := com.getArgPos(com.pc+2, bMode)
-			if err != nil {
-				return 0, false, err
-			}
-			o, err := com.getArgPos(com.pc+3, cMode)
-			if err != nil {
-				return 0, false, err
-			}
-			if com.mem[a] == com.mem[b] {
-				com.mem[o] = 1
-			} else {
-				com.mem[o] = 0
-			}
-			com.pc += 4
-		case OpRelativeBase:
-			a, err := com.getArgPos(com.pc+1, aMode)
-			if err != nil {
-				return 0, false, err
-			}
-			com.rb += com.mem[a]
-			com.pc += 2
-		default:
-			return 0, false, fmt.Errorf("invalid op: %d", op)
-		}
-	}
-	return 0, false, fmt.Errorf("fell out of memory.. pc@%d, max mem: %d", com.pc, len(com.mem))
-}
 
 func main() {
 	input, err := os.Open("input.txt")
@@ -250,7 +38,7 @@ func main() {
 	}
 
 	// Part 1
-	com := NewComputer(ops)
+	com := intcode.NewComputer(ops)
 	fmt.Printf("Part 1:\n")
 	for !com.Halted {
 		out, halt, err := com.Iterate(func() int {
@@ -265,7 +53,7 @@ func main() {
 	}
 
 	// Part 2
-	com = NewComputer(ops)
+	com = intcode.NewComputer(ops)
 	fmt.Printf("Part 2:\n")
 	for !com.Halted {
 		out, halt, err := com.Iterate(func() int {
