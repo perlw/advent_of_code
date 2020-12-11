fcfc9fbcf221d9631d71397469936c88054c398c
Per Lönn Wege <per@perlw.se>
2020-12-11 11:31:09 +0100

    2020/11 skeleton

---
+++ 2020/11/main.go
@@ -0,0 +1,29 @@
+package main
+
+// Task1 ...
+func Task1() {
+}
+
+// Task2 ...
+func Task2() {
+}
+
+func main() {
+	/*
+		file, _ := os.Open("input.txt")
+		scanner := bufio.NewScanner(file)
+
+		input := make([]int, 0, 100)
+		for scanner.Scan() {
+			val, _ := strconv.Atoi(scanner.Text())
+			input = append(input, val)
+		}
+		file.Close()
+
+		result := Task1(input)
+		fmt.Printf("Task 1: %d\n", result)
+
+		result = Task2(input)
+		fmt.Printf("Task 2: %d\n", result)
+	*/
+}
---
+++ 2020/11/main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestTask1ShouldFindResult(t *testing.T) {
+}
+
+func TestTask2ShouldFindResult(t *testing.T) {
+}
