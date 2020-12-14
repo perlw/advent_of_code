c622edd53feeb86047dced9a824e84727e452dd1
Per Lönn Wege <per@perlw.se>
2020-12-14 15:54:49 +0100

    2020/13

--- 2020/13/main.go
+++ 2020/13/main.go
@@ -21,11 +21,15 @@ func (b Bus) NextDeparture(now int) int {
 func Task1(now int, input []Bus) int {
 	soonestDeparture := math.MaxInt32
 	var soonestBusID int
-	for _, i := range input {
-		next := i.NextDeparture(now)
+	for _, bus := range input {
+		if bus == -1 {
+			continue
+		}
+
+		next := bus.NextDeparture(now)
 		if next < soonestDeparture {
 			soonestDeparture = next
-			soonestBusID = int(i)
+			soonestBusID = int(bus)
 		}
 	}
 
@@ -33,8 +37,25 @@ func Task1(now int, input []Bus) int {
 }
 
 // Task2 ...
-func Task2() int {
-	return -1
+func Task2(input []Bus) int {
+	timestamp := 1
+	increment := 1
+
+	for i, bus := range input {
+		if bus == -1 {
+			continue
+		}
+
+		for {
+			if (timestamp+i)%int(bus) == 0 {
+				increment *= int(bus)
+				break
+			}
+			timestamp += increment
+		}
+	}
+
+	return timestamp
 }
 
 func main() {
@@ -47,16 +68,19 @@ func main() {
 	scanner.Scan()
 	buses := strings.Split(scanner.Text(), ",")
 	for _, bus := range buses {
-		if bus != "x" {
-			t, _ := strconv.Atoi(bus)
-			input = append(input, Bus(t))
+		var val int
+		if bus == "x" {
+			val = -1
+		} else {
+			val, _ = strconv.Atoi(bus)
 		}
+		input = append(input, Bus(val))
 	}
 	file.Close()
 
 	result := Task1(now, input)
 	fmt.Printf("Task 1: %d\n", result)
 
-	result = Task2()
+	result = Task2(input)
 	fmt.Printf("Task 2: %d\n", result)
 }
--- 2020/13/main_test.go
+++ 2020/13/main_test.go
@@ -19,7 +19,7 @@ func TestBusShouldHaveTimetable(t *testing.T) {
 
 func TestTask1ShouldFindResult(t *testing.T) {
 	start := 939
-	input := []Bus{7, 13, 19, 31, 59}
+	input := []Bus{7, 13, -1, -1, 59, -1, 31, 19}
 	expected := 295
 
 	result := Task1(start, input)
@@ -29,4 +29,20 @@ func TestTask1ShouldFindResult(t *testing.T) {
 }
 
 func TestTask2ShouldFindResult(t *testing.T) {
+	tests := [][]Bus{
+		{7, 13, -1, -1, 59, -1, 31, 19},
+		{17, -1, 13, 19},
+		{67, 7, 59, 61},
+		{67, -1, 7, 59, 61},
+		{67, 7, -1, 59, 61},
+		{1789, 37, 47, 1889},
+	}
+	expected := []int{1068781, 3417, 754018, 779210, 1261476, 1202161486}
+
+	for i, test := range tests {
+		result := Task2(test)
+		if result != expected[i] {
+			t.Errorf("%d: got %d, expected %d", i, result, expected)
+		}
+	}
 }
