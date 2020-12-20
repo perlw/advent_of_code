738d0a6f355d888f080fe09b399ed983a141ea86
Per Lönn Wege <per@perlw.se>
2020-12-20 20:24:28 +0100

    2020/19 wip

--- 2020/19/main.go
+++ 2020/19/main.go
@@ -10,19 +10,133 @@ import (
 
 // Rule ...
 type Rule struct {
-	R     rune
-	Match [][]int
+	R rune
+	M [][]int
+}
+
+var ruleCache = make(map[int][]string)
+
+func resetCache() {
+	ruleCache = make(map[int][]string)
+}
+
+func generateValidMessages(rules map[int]Rule, index int, debug bool) []string {
+	if debug {
+		fmt.Printf("----\n")
+	}
+	if c, ok := ruleCache[index]; ok {
+		return c
+	}
+	result := make([]string, 0, 10)
+	for _, m := range rules[index].M {
+		current := make([]string, 0, 10)
+
+		for _, i := range m {
+			if rules[i].R > 0 {
+				alt := string(rules[i].R)
+				if debug {
+					fmt.Printf("adding %s to %+v\n", alt, current)
+				}
+				if len(current) == 0 {
+					current = append(current, alt)
+				} else {
+					for i := 0; i < len(current); i++ {
+						current[i] += alt
+					}
+				}
+				if debug {
+					fmt.Printf("now %+v\n", current)
+				}
+			} else {
+				tmp := make([]string, 0, 10)
+				parts := generateValidMessages(rules, i, debug)
+				if debug {
+					fmt.Printf("current %+v parts %+v\n", current, parts)
+				}
+				for _, p := range parts {
+					if len(current) == 0 {
+						tmp = append(tmp, p)
+					} else {
+						for _, c := range current {
+							tmp = append(tmp, c+p)
+						}
+					}
+				}
+				current = tmp
+
+				if debug {
+					fmt.Printf("new current %+v\n", current)
+				}
+			}
+		}
+
+		for _, c := range current {
+			result = append(result, c)
+		}
+		ruleCache[index] = result
+	}
+	ruleCache[index] = result
+	return result
 }
 
 // Task1 ...
-func Task1(rules map[int]Rule, messages []string) int {
+func Task1(rules map[int]Rule, messages []string, debug bool) int {
+	valid := generateValidMessages(rules, 0, debug)
+	fmt.Printf("Valid rules: %d\n", len(valid))
+
 	var result int
+	for _, m := range messages {
+		var found bool
+		for _, v := range valid {
+			if m == v {
+				found = true
+				fmt.Printf("%s found!\n", m)
+				result++
+				break
+			}
+		}
+		if !found {
+			fmt.Printf("%s missing...\n", m)
+		}
+	}
+
 	return result
 }
 
 // Task2 ...
-func Task2() int {
+func Task2(rules map[int]Rule, messages []string, debug bool) int {
+	rules[8] = Rule{
+		M: [][]int{
+			{42},
+			{42, 8},
+		},
+	}
+	rules[11] = Rule{
+		M: [][]int{
+			{42, 31},
+			{42, 11, 31},
+		},
+	}
+
+	valid := generateValidMessages(rules, 0, debug)
+	fmt.Printf("Valid rules: %d\n", len(valid))
+
 	var result int
+	for _, m := range messages {
+		var found bool
+		for _, v := range valid {
+			if m == v {
+				found = true
+				fmt.Printf("%s found!\n", m)
+				result++
+				break
+			}
+		}
+		if !found {
+			fmt.Printf("%s missing...\n", m)
+		}
+	}
+
 	return result
 }
 
@@ -43,7 +157,7 @@ func main() {
 		matchParts := strings.Split(parts[1], " | ")
 
 		rule := Rule{
-			Match: make([][]int, 0, 10),
+			M: make([][]int, 0, 10),
 		}
 		if matchParts[0][0] == '"' {
 			rule.R = rune(matchParts[0][1])
@@ -54,7 +168,7 @@ func main() {
 				v, _ := strconv.Atoi(str)
 				match = append(match, v)
 			}
-			rule.Match = append(rule.Match, match)
+			rule.M = append(rule.M, match)
 		}
 		rules[i] = rule
 	}
@@ -64,9 +178,11 @@ func main() {
 	}
 	file.Close()
 
-	result := Task1(rules, messages)
+	resetCache()
+	result := Task1(rules, messages, false)
 	fmt.Printf("Task 1: %d\n", result)
 
-	result = Task2()
+	resetCache()
+	result = Task2(rules, messages, false)
 	fmt.Printf("Task 2: %d\n", result)
 }
--- 2020/19/main_test.go
+++ 2020/19/main_test.go
@@ -2,27 +2,171 @@ package main
 
 import "testing"
 
-func TestTask1ShouldFindResult(t *testing.T) {
+func TestShouldGenerateValidMessagesSimple(t *testing.T) {
 	rules := map[int]Rule{
 		0: {
-			Match: [][]int{
+			M: [][]int{
+				{1, 2},
+			},
+		},
+		1: {
+			R: 'a',
+		},
+		2: {
+			M: [][]int{
+				{1, 3},
+				{3, 1},
+			},
+		},
+		3: {
+			R: 'b',
+		},
+	}
+	expected := []string{
+		"aab",
+		"aba",
+	}
+
+	resetCache()
+	result := generateValidMessages(rules, 0, true)
+	if len(result) != len(expected) {
+		t.Errorf("got %+v, expected %+v", result, expected)
+		return
+	}
+	for i := range result {
+		if result[i] != expected[i] {
+			t.Errorf("got %+v, expected %+v", result, expected)
+		}
+	}
+}
+
+func TestShouldGenerateValidMessagesComplex(t *testing.T) {
+	rules := map[int]Rule{
+		0: {
+			M: [][]int{
 				{4, 1, 5},
 			},
 		},
 		1: {
-			Match: [][]int{
+			M: [][]int{
 				{2, 3},
 				{3, 2},
 			},
 		},
 		2: {
-			Match: [][]int{
+			M: [][]int{
 				{4, 4},
 				{5, 5},
 			},
 		},
 		3: {
-			Match: [][]int{
+			M: [][]int{
+				{4, 5},
+				{5, 4},
+			},
+		},
+		4: {
+			R: 'a',
+		},
+		5: {
+			R: 'b',
+		},
+	}
+	expected := []string{
+		"aaaabb",
+		"aaabab",
+		"abbabb",
+		"abbbab",
+		"aabaab",
+		"aabbbb",
+		"abaaab",
+		"ababbb",
+	}
+
+	resetCache()
+	result := generateValidMessages(rules, 0, true)
+	if len(result) != len(expected) {
+		t.Errorf("got %+v, expected %+v", result, expected)
+		return
+	}
+	for _, i := range expected {
+		var found bool
+		for _, j := range result {
+			if i == j {
+				found = true
+				break
+			}
+		}
+		if !found {
+			t.Errorf("got %+v, expected %+v", result, expected)
+		}
+	}
+}
+
+func TestShouldGenerateValidMessagesLoop(t *testing.T) {
+	rules := map[int]Rule{
+		0: {
+			M: [][]int{
+				{1, 2},
+			},
+		},
+		1: {
+			M: [][]int{
+				{3},
+				{3, 1},
+			},
+		},
+		2: {
+			M: [][]int{
+				{3, 4},
+				{3, 1},
+			},
+		},
+		3: {
+			R: 'a',
+		},
+		4: {
+			R: 'b',
+		},
+	}
+	expected := []string{
+		"aaab",
+	}
+
+	resetCache()
+	result := generateValidMessages(rules, 0, true)
+	if len(result) != len(expected) {
+		t.Errorf("got %+v, expected %+v", result, expected)
+		return
+	}
+	for i := range result {
+		if result[i] != expected[i] {
+			t.Errorf("got %+v, expected %+v", result, expected)
+		}
+	}
+}
+
+func TestTask1ShouldFindResult(t *testing.T) {
+	rules := map[int]Rule{
+		0: {
+			M: [][]int{
+				{4, 1, 5},
+			},
+		},
+		1: {
+			M: [][]int{
+				{2, 3},
+				{3, 2},
+			},
+		},
+		2: {
+			M: [][]int{
+				{4, 4},
+				{5, 5},
+			},
+		},
+		3: {
+			M: [][]int{
 				{4, 5},
 				{5, 4},
 			},
@@ -43,16 +187,209 @@ func TestTask1ShouldFindResult(t *testing.T) {
 	}
 	expected := 2
 
-	result := Task1(rules, messages)
+	resetCache()
+	result := Task1(rules, messages, true)
 	if result != expected {
 		t.Errorf("got %d, expected %d", result, expected)
 	}
 }
 
 func TestTask2ShouldFindResult(t *testing.T) {
-	expected := -1
+	rules := map[int]Rule{
+		42: {
+			M: [][]int{{
+				9, 14},
+				{10, 1},
+			},
+		},
+		9: {
+			M: [][]int{
+				{14, 27},
+				{1, 26},
+			},
+		},
+		10: {
+			M: [][]int{
+				{23, 14},
+				{28, 1},
+			},
+		},
+		1: {
+			R: 'a',
+		},
+		11: {
+			M: [][]int{
+				{42, 31},
+			},
+		},
+		5: {
+			M: [][]int{
+				{1, 14},
+				{15, 1},
+			},
+		},
+		19: {
+			M: [][]int{
+				{14, 1},
+				{14, 14},
+			},
+		},
+		12: {
+			M: [][]int{
+				{24, 14},
+				{19, 1},
+			},
+		},
+		16: {
+			M: [][]int{
+				{15, 1},
+				{14, 14},
+			},
+		},
+		31: {
+			M: [][]int{
+				{14, 17},
+				{1, 13},
+			},
+		},
+		6: {
+			M: [][]int{
+				{14, 14},
+				{1, 14},
+			},
+		},
+		2: {
+			M: [][]int{
+				{1, 24},
+				{14, 4},
+			},
+		},
+		0: {
+			M: [][]int{
+				{8, 11},
+			},
+		},
+		13: {
+			M: [][]int{
+				{14, 3},
+				{1, 12},
+			},
+		},
+		15: {
+			M: [][]int{
+				{1},
+				{14},
+			},
+		},
+		17: {
+			M: [][]int{
+				{14, 2},
+				{1, 7},
+			},
+		},
+		23: {
+			M: [][]int{
+				{25, 1},
+				{22, 14},
+			},
+		},
+		28: {
+			M: [][]int{
+				{16, 1},
+			},
+		},
+		4: {
+			M: [][]int{
+				{1, 1},
+			},
+		},
+		20: {
+			M: [][]int{
+				{14, 14},
+				{1, 15},
+			},
+		},
+		3: {
+			M: [][]int{
+				{5, 14},
+				{16, 1},
+			},
+		},
+		27: {
+			M: [][]int{
+				{1, 6},
+				{14, 18},
+			},
+		},
+		14: {
+			R: 'b',
+		},
+		21: {
+			M: [][]int{
+				{14, 1},
+				{1, 14},
+			},
+		},
+		25: {
+			M: [][]int{
+				{1, 1},
+				{1, 14},
+			},
+		},
+		22: {
+			M: [][]int{
+				{14, 14},
+			},
+		},
+		8: {
+			M: [][]int{
+				{42},
+			},
+		},
+		26: {
+			M: [][]int{
+				{14, 22},
+				{1, 20},
+			},
+		},
+		18: {
+			M: [][]int{
+				{15, 15},
+			},
+		},
+		7: {
+			M: [][]int{
+				{14, 5},
+				{1, 21},
+			},
+		},
+		24: {
+			M: [][]int{
+				{14, 1},
+			},
+		},
+	}
+	messages := []string{
+		"abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa",
+		"bbabbbbaabaabba",
+		"babbbbaabbbbbabbbbbbaabaaabaaa",
+		"aaabbbbbbaaaabaababaabababbabaaabbababababaaa",
+		"bbbbbbbaaaabbbbaaabbabaaa",
+		"bbbababbbbaaaaaaaabbababaaababaabab",
+		"ababaaaaaabaaab",
+		"ababaaaaabbbaba",
+		"baabbaaaabbaaaababbaababb",
+		"abbbbabbbbaaaababbbbbbaaaababb",
+		"aaaaabbaabaaaaababaa",
+		"aaaabbaaaabbaaa",
+		"aaaabbaabbaaaaaaabbbabbbaaabbaabaaa",
+		"babaaabbbaaabaababbaabababaaab",
+		"aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba",
+	}
+	expected := 12
 
-	result := Task2()
+	resetCache()
+	result := Task2(rules, messages, false)
 	if result != expected {
 		t.Errorf("got %d, expected %d", result, expected)
 	}
