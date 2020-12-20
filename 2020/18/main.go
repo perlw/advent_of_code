d93d7b21ce03845e888e6279f53fbd6279bcd0b7
Per Lönn Wege <per@perlw.se>
2020-12-20 14:25:23 +0100

    2020/18

--- 2020/18/main.go
+++ 2020/18/main.go
@@ -1,3 +1,12 @@
+// Note: Do NOT use this solutions as a good example. I hate it. It's a horrible,
+// mess of brute force, workarounds, and shortcuts, and is generally really not
+// a good idea. A syntax tree would be a better idea and what I should have done
+// from the beginning (but I tried to save time by using recursion instead).
+// I've fought overflows, indexing errors, recursion loops, general stubborn
+// stupidity, and I'm even two days behind.
+// But I guess I've learnt what _not_ to do as well.
+// Next year AoC, next year..
+
 package main
 
 import (
@@ -9,17 +18,25 @@ import (
 
 func reduce(exp []rune, debug bool) int {
 	if debug {
-		fmt.Printf("iter %s\n", string(exp))
+		fmt.Printf("iter ")
+		for _, r := range exp {
+			if r < ' ' {
+				fmt.Printf("%d", r)
+			} else {
+				fmt.Printf("%c", r)
+			}
+		}
+		fmt.Println()
 	}
 	var a, b int
 	var op rune
 
 	if len(exp) == 1 {
-		return int(exp[0] - '0')
+		return int(exp[0])
 	}
 	if len(exp) <= 3 {
-		a = int(exp[0] - '0')
-		b = int(exp[2] - '0')
+		a = int(exp[0])
+		b = int(exp[2])
 		op = exp[1]
 
 		if debug {
@@ -36,18 +53,18 @@ func reduce(exp []rune, debug bool) int {
 					if count == 0 {
 						if i == 0 {
 							return reduce(exp[i+1:len(exp)-1], debug)
-						} else {
-							a = reduce(exp[:i-1], debug)
-							b = reduce(exp[i+1:len(exp)-1], debug)
-							op = exp[i-1]
 						}
+
+						a = reduce(exp[:i-1], debug)
+						b = reduce(exp[i+1:len(exp)-1], debug)
+						op = exp[i-1]
 						break
 					}
 				}
 			}
 		} else {
 			a = reduce(exp[:len(exp)-2], debug)
-			b = int(exp[len(exp)-1] - '0')
+			b = int(exp[len(exp)-1])
 			op = exp[len(exp)-2]
 		}
 
@@ -70,18 +87,203 @@ func reduce(exp []rune, debug bool) int {
 	return result
 }
 
+func reduceAdditions(exp []int, debug bool) []int {
+	newExp := make([]int, 0, len(exp))
+
+	for i := 0; i < len(exp); i++ {
+		switch exp[i] {
+		case '(':
+			var count int
+			for j := i; j < len(exp); j++ {
+				if exp[j] == '(' {
+					count++
+				} else if exp[j] == ')' {
+					count--
+					if count == 0 {
+						if debug {
+							fmt.Printf("mm ")
+							for _, r := range exp[i+1 : j] {
+								if r == '*' || r == '+' || r == '(' || r == ')' {
+									fmt.Printf("%c", r)
+								} else {
+									fmt.Printf("%d", r-255)
+								}
+							}
+							fmt.Println()
+						}
+						e := reduceAdditions(exp[i+1:j], debug)
+						for {
+							prev := e
+							e = reduceAdditions(e, debug)
+							if len(prev) == len(e) {
+								break
+							}
+						}
+
+						if debug {
+							fmt.Printf("++ ")
+							for _, r := range e {
+								if r == '*' || r == '+' || r == '(' || r == ')' {
+									fmt.Printf("%c", r)
+								} else {
+									fmt.Printf("%d", r-255)
+								}
+							}
+							fmt.Println()
+						}
+
+						e = reduceMultiplications(e, debug)
+						for {
+							prev := e
+							if len(prev) == len(e) {
+								break
+							}
+						}
+						if debug {
+							fmt.Printf("** ")
+							for _, r := range e {
+								if r == '*' || r == '+' || r == '(' || r == ')' {
+									fmt.Printf("%c", r)
+								} else {
+									fmt.Printf("%d", r-255)
+								}
+							}
+							fmt.Println()
+						}
+						if len(e) == 1 {
+							newExp = append(newExp, e...)
+						} else {
+							newExp = append(newExp, '(')
+							newExp = append(newExp, e...)
+							newExp = append(newExp, ')')
+						}
+
+						i = j
+						break
+					}
+				}
+			}
+		case '*':
+			newExp = append(newExp, exp[i])
+		case '+':
+			if exp[i-1] >= 255 && exp[i+1] >= 255 {
+				if debug {
+					fmt.Printf("add %d %d\n", newExp[len(newExp)-1]-255, exp[i+1]-255)
+				}
+				newExp[len(newExp)-1] = ((newExp[len(newExp)-1] - 255) + (exp[i+1] - 255)) + 255
+				i++
+			} else {
+				newExp = append(newExp, exp[i])
+			}
+		default:
+			newExp = append(newExp, exp[i])
+		}
+	}
+
+	return newExp
+}
+
+func reduceMultiplications(exp []int, debug bool) []int {
+	newExp := make([]int, 0, len(exp))
+
+	for i := 0; i < len(exp); i++ {
+		switch exp[i] {
+		case '*':
+			if exp[i-1] >= 255 && exp[i+1] >= 255 {
+				newExp[len(newExp)-1] = ((newExp[len(newExp)-1] - 255) * (exp[i+1] - 255)) + 255
+				i++
+			} else {
+				newExp = append(newExp, exp[i])
+			}
+		default:
+			newExp = append(newExp, exp[i])
+		}
+	}
+
+	return newExp
+}
+
+func reduce2(exp []int, debug bool) int {
+	exp = reduceAdditions(exp, debug)
+	for {
+		prev := exp
+		exp = reduceAdditions(exp, debug)
+		if debug {
+			fmt.Printf("additions ")
+			for _, r := range exp {
+				if r == '*' || r == '+' || r == '(' || r == ')' {
+					fmt.Printf("%c", r)
+				} else {
+					fmt.Printf("%d", r-255)
+				}
+			}
+			fmt.Println()
+		}
+
+		if len(prev) == len(exp) {
+			break
+		}
+	}
+
+	exp = reduceMultiplications(exp, debug)
+	for {
+		prev := exp
+		exp = reduceMultiplications(exp, debug)
+		if debug {
+			fmt.Printf("multiplications ")
+			for _, r := range exp {
+				if r == '*' || r == '+' || r == '(' || r == ')' {
+					fmt.Printf("%c", r)
+				} else {
+					fmt.Printf("%d", r-255)
+				}
+			}
+			fmt.Println()
+		}
+
+		if len(prev) == len(exp) {
+			break
+		}
+	}
+
+	return exp[0] - 255
+}
+
+func prepareInput(str string, fudge bool) []rune {
+	line := []rune(strings.ReplaceAll(str, " ", ""))
+	for i, r := range line {
+		if r != '(' && r != ')' && r != '*' && r != '+' {
+			if fudge {
+				line[i] = 255 + r - '0'
+			} else {
+				line[i] = r - '0'
+			}
+		}
+	}
+	return line
+}
+
 // Task1 ...
-func Task1(input []string) int {
+func Task1(input [][]rune) int {
 	var result int
 	for i := range input {
-		result += reduce([]rune(input[i]), false)
+		result += reduce(input[i], false)
 	}
 	return result
 }
 
 // Task2 ...
-func Task2() int {
-	return -1
+func Task2(input [][]rune) int {
+	var result int
+	for i := range input {
+		in := make([]int, len(input[i]))
+		for i, j := range input[i] {
+			in[i] = int(j)
+		}
+		r := reduce2(in, false)
+		result += r
+	}
+	return result
 }
 
 func main() {
@@ -90,14 +292,21 @@ func main() {
 
 	input := make([]string, 0, 100)
 	for scanner.Scan() {
-		line := scanner.Text()
-		input = append(input, strings.ReplaceAll(line, " ", ""))
+		input = append(input, scanner.Text())
 	}
 	file.Close()
 
-	result := Task1(input)
+	in := make([][]rune, len(input))
+	for i, line := range input {
+		in[i] = prepareInput(line, false)
+	}
+	result := Task1(in)
 	fmt.Printf("Task 1: %d\n", result)
 
-	result = Task2()
+	in = make([][]rune, len(input))
+	for i, line := range input {
+		in[i] = prepareInput(line, true)
+	}
+	result = Task2(in)
 	fmt.Printf("Task 2: %d\n", result)
 }
--- 2020/18/main_test.go
+++ 2020/18/main_test.go
@@ -26,7 +26,7 @@ func TestShouldReduceExpression(t *testing.T) {
 	}
 
 	for i, test := range tests {
-		result := reduce([]rune(test), true)
+		result := reduce(prepareInput(test, false), true)
 		fmt.Printf("[[%s => %d]]\n", test, result)
 		fmt.Println()
 		if result != expected[i] {
@@ -35,11 +35,45 @@ func TestShouldReduceExpression(t *testing.T) {
 	}
 }
 
-func TestTask2ShouldFindResult(t *testing.T) {
-	expected := -1
+func TestShouldReduceExpression2(t *testing.T) {
+	tests := []string{
+		"1+2*3+4*5+6",
+		"2*3+(4+5)",
+		"2*3+(4*5)",
+		"(2*3)+(4*5)",
+		"(2*3)*(4*5)",
+		"1+(2+1)",
+		"1+(2*3)+(4*(5+6))",
+		"2*3+(4*5)",
+		"5+(8*3+9+3*4*3)",
+		"5*9*(7*3*3+9*3+(8+6*4))",
+		"((2+4*9)*(6+9*8+6)+6)+2+4*2",
+	}
+	expected := []int{
+		231,
+		24,
+		46,
+		26,
+		120,
+		4,
+		51,
+		46,
+		1445,
+		669060,
+		23340,
+	}
 
-	result := Task2()
-	if result != expected {
-		t.Errorf("got %d, expected %d", result, expected)
+	for i, test := range tests {
+		tst := prepareInput(test, true)
+		in := make([]int, len(tst))
+		for i, j := range tst {
+			in[i] = int(j)
+		}
+		result := reduce2(in, true)
+		fmt.Printf("[[%s => %d]]\n", test, result)
+		fmt.Println()
+		if result != expected[i] {
+			t.Errorf("%d: got %d, expected %d", i, result, expected[i])
+		}
 	}
 }
