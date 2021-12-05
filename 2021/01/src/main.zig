6ac3a415c952bd3bb0a7c0e26eb4076b467e75a2
Per Lönn Wege <per@perlw.se>
2021-12-05 14:52:30 +0100

    2021/01 part 2

--- 2021/01/src/main.zig
+++ 2021/01/src/main.zig
@@ -25,15 +25,42 @@ fn readInputFile(allocator: *std.mem.Allocator, filename: []const u8) anyerror![
 }
 
 pub fn task1(depth_values: []const u32) u32 {
-    var count: u32 = 0;
+    var result: u32 = 0;
     var prev = depth_values[0];
     for (depth_values) |depth| {
         if (depth > prev) {
-            count += 1;
+            result += 1;
         }
         prev = depth;
     }
-    return count;
+    return result;
+}
+
+pub fn task2(depth_values: []const u32) u32 {
+    var result: u32 = 0;
+    var prev_batch: u32 = 0;
+    var batch_index: u32 = 0;
+    var batch_value: u32 = 0;
+
+    var count: u32 = 0;
+    while (batch_index < depth_values.len) {
+        batch_value += depth_values[batch_index];
+        batch_index += 1;
+        count += 1;
+
+        if (count == 3) {
+            if (prev_batch > 0 and batch_value > prev_batch) {
+                result += 1;
+            }
+
+            batch_index -= 2;
+            prev_batch = batch_value;
+            count = 0;
+            batch_value = 0;
+        }
+    }
+
+    return result;
 }
 
 pub fn main() anyerror!void {
@@ -41,8 +68,10 @@ pub fn main() anyerror!void {
     const allocator = &std.heap.FixedBufferAllocator.init(&buffer).allocator;
 
     const input = try readInputFile(allocator, "input.txt");
-    std.log.debug("input {any}", .{input});
 
     const task_1_result = task1(input);
     std.log.info("Task 1 result: {}", .{task_1_result});
+
+    const task_2_result = task2(input);
+    std.log.info("Task 2 result: {}", .{task_2_result});
 }
--- 2021/01/src/main_test.zig
+++ 2021/01/src/main_test.zig
@@ -19,3 +19,20 @@ test "expect task 1 to yield correct result" {
 
     try std.testing.expect(app.task1(input[0..]) == expected);
 }
+
+test "expect task 2 to yield correct result" {
+    const input = [_]u32{
+        199,
+        200,
+        208,
+        210,
+        200,
+        207,
+        240,
+        269,
+        260,
+        263,
+    };
+    const expected: u32 = 5;
+    try std.testing.expect(app.task2(input[0..]) == expected);
+}
