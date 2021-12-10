1eb31a093cbadfff2b4304f3b07162efd0caa4aa
Per Lönn Wege <per@perlw.se>
2021-12-10 21:47:50 +0100

    2021/09 part 2

--- 2021/09/src/main.zig
+++ 2021/09/src/main.zig
@@ -46,7 +46,7 @@ pub fn task1(width: u32, height: u32, input: []u32) u32 {
             }
 
             if (is_low_point) {
-                std.log.debug("low point->{}", .{depth});
+                // std.log.debug("low point->{}", .{depth});
                 result += depth + 1;
             }
         }
@@ -55,8 +55,58 @@ pub fn task1(width: u32, height: u32, input: []u32) u32 {
     return result;
 }
 
-pub fn task2(input: []u32) u32 {
-    var result: u32 = 0;
+fn drawGrid(width: u32, height: u32, grid: []u32) void {
+    var y: u32 = 0;
+    std.log.debug("grid", .{});
+    while (y < height) : (y += 1) {
+        std.log.debug("{any}", .{grid[(y * width)..((y + 1) * width)]});
+    }
+}
+
+fn flood(x: u32, y: u32, width: u32, height: u32, grid: []u32) u32 {
+    var result: u32 = 1;
+    const j = (y * width) + x;
+    grid[j] = 9;
+    if (y > 0 and grid[j - width] != 9) {
+        result += flood(x, y - 1, width, height, grid);
+    }
+    if (y < height - 1 and grid[j + width] != 9) {
+        result += flood(x, y + 1, width, height, grid);
+    }
+    if (x > 0 and grid[j - 1] != 9) {
+        result += flood(x - 1, y, width, height, grid);
+    }
+    if (x < width - 1 and grid[j + 1] != 9) {
+        result += flood(x + 1, y, width, height, grid);
+    }
+    return result;
+}
+
+const desc_u32 = std.sort.desc(u32);
+
+pub fn task2(allocator: *std.mem.Allocator, width: u32, height: u32, input: []u32) !u32 {
+    var result: u32 = 1;
+
+    var basins = std.ArrayList(u32).init(allocator);
+    defer basins.clearAndFree();
+
+    var y: u32 = 0;
+    while (y < height) : (y += 1) {
+        var i = y * width;
+        var x: u32 = 0;
+        while (x < width) : (x += 1) {
+            if (input[i + x] != 9) {
+                const basin = flood(x, y, width, height, input);
+                // drawGrid(width, height, input);
+                try basins.append(basin);
+            }
+        }
+    }
+
+    std.sort.sort(u32, basins.items, {}, desc_u32);
+    for (basins.items[0..3]) |i| {
+        result *= i;
+    }
 
     return result;
 }
@@ -71,6 +121,6 @@ pub fn main() !void {
     const task_1_result = task1(100, 100, input);
     std.log.info("Task 1 result: {}", .{task_1_result});
 
-    const task_2_result = task2(input);
-    std.log.info("Task 1 result: {}", .{task_2_result});
+    const task_2_result = task2(allocator, 100, 100, input);
+    std.log.info("Task 2 result: {}", .{task_2_result});
 }
--- 2021/09/src/main_test.zig
+++ 2021/09/src/main_test.zig
@@ -16,3 +16,18 @@ test "expect task 1 to sum to 15" {
 
     try std.testing.expect(app.task1(10, 5, &input) == expected);
 }
+
+test "expect task 2 to sum to 1134" {
+    var input = [_]u32{
+        2, 1, 9, 9, 9, 4, 3, 2, 1, 0,
+        3, 9, 8, 7, 8, 9, 4, 9, 2, 1,
+        9, 8, 5, 6, 7, 8, 9, 8, 9, 2,
+        8, 7, 6, 7, 8, 9, 6, 7, 8, 9,
+        9, 8, 9, 9, 9, 6, 5, 6, 7, 8,
+    };
+    const expected: u32 = 1134;
+
+    std.testing.log_level = .debug;
+
+    try std.testing.expect((try app.task2(std.testing.allocator, 10, 5, &input)) == expected);
+}
